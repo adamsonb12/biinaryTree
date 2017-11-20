c5e9be232db836b25882f4ad4736fea163d22005
Adamson <AdamsonBH@ad.mtc.byu.edu>
2017-11-20 14:09:38 -0700

    started creating node class

--- node.py
+++ node.py
@@ -8,3 +8,18 @@
 # 1. `left` - a reference to the left child of the node.
 # 1. `right` - a reference to the right child of the node.
 
+class Node(object):
+    '''A node in a binary tree'''
+
+    def __init__(self, key, value, parent=None, left=None, right=None):
+        self.key = key
+        self.value = value
+        self.parent = parent
+        self.left = left
+        self.right = right
+        
+    def __str__(self):
+        return '<Node: {}:{}>'.format(self.key, self.value)
+
+    
+
---
+++ test.py
@@ -0,0 +1,4 @@
+from Node import Node
+
+n = Node(1, 'whatever I want')
+n.__str__()
